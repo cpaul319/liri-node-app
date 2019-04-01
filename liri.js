18e8cec5b9e8dee70a8404418ab838ff0fc14540
Chris Paul <cpaul319@users.noreply.github.com>
2019-04-01 19:04:41 -0400

    update

--- liri.js
+++ liri.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
+ 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify); 
+var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var moment = require("moment");
 var fs = require("fs");
  
+
  
 var command = process.argv[2];
 var parameter = process.argv.slice(3).join(" ");
